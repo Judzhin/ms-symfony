4b87b32f97f7f42f97833c7c6544d0d40fbfd7fa
Judzhin Miles <info@msbios.com>
2019-07-24 12:58:23 +0300

    Init Symfony 4

---
+++ src/Controller/MainController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class MainController
+ * @package App\Controller
+ */
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="main")
+     */
+    public function index()
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/MainController.php',
+        ]);
+    }
+}
